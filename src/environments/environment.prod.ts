ef6fda87621bbec8e7379686e38249d4faa29a5f
afdaliable <arazakibsr@gmail.com>
2025-03-12 20:59:26 +0700

    fix url prod, cp 1.1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8787/api',
+  apiUrl: '/api',
   withCredentials: true,
   corsConfig: {
-    allowedOrigins: ['https://your-production-domain.com', 'http://localhost:4200'],
+    allowedOrigins: ['https://kuis.canducation.com'],
     allowedMethods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['*'],
     exposedHeaders: ['Authorization'],
@@ -13,3 +13,5 @@ export const environment = {
   // Replace this with your actual Google Client ID
   googleClientId: '122895789472-5ppmksj8nnha9nhlktuikl4tggdal0pf.apps.googleusercontent.com'
 };   
+
+  
